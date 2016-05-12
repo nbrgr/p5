996ac78fccec2d139ce4c266e75788681b365850
brgr-dev <berger1954@gmail.com>
2016-05-12 11:29:14 -0500

    Added code to print fs info

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -15,6 +15,9 @@ int main(int argc, char* argv[]) {
 	struct superblock* superblock;
 	int ninodes;
 	int ninodeblocks;
+	int bitmaps;
+	int nbitmapblocks;
+	int ndatablocks;
 
 	if(argc != 2)
 	{
@@ -42,10 +45,22 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	blocks = (struct block*)map;
-	superblock = (struct superblock*)blocks[1];
-	ninodes = superblock->ninodes;
-	ninodeblocks = ninodes / IPB;
+	blocks = (struct block*)map; // Converts our mapping into an array of blocks;
+
+	superblock = (struct superblock*)blocks[1]; // Gets a pointer to the superblock;
+
+	ninodes = superblock->ninodes; // Gets the number of inodes in the system;
+
+	ninodeblocks = ninodes / IPB; // Computes the number of inode blocks there are;
+
+	bitmaps = BBLOCK(0, ninodes); // Finds the first block number of the bitmaps 
+
+	ndatablocks = superblock->ndatablocks; // Gets the number of datablocks there are
+
+	nbitmapblocks = ndatablocks / (BSIZE * BPB); // Computes the number of bit map blocks there are.
+
+	printf("Number of inodes: %d, Number of inode blocks: %d\n", ninodes, ninodeblocks);
+	printf("Location of bitmaps: %d, Number of data blocks: %d, Number of bitmap blocks: %d\n", bitmaps, ndatablocks, nbitmapblocks);
 
     return 0;
 }
