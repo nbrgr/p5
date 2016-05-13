c5ee498763ed514dfaaf7c006856e7c366159bf8
kbstoner <kstoner246@gmail.com>
2016-05-13 09:06:32 -0500

    Update fscheck.c

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -78,7 +78,7 @@ int main(int argc, char* argv[]) {
 	for(i = 0; i < ninodes; i++)
 	{
 		//fprintf(stderr, "inode type: %i\n", inodes[i].type);
-		if(inodes[i] != NULL && (inodes[i].type != T_DIR) && (inodes[i].type != T_FILE) && (inodes[i].type != T_DEV))
+		if(&inodes[i] != NULL && (inodes[i].type != T_DIR) && (inodes[i].type != T_FILE) && (inodes[i].type != T_DEV))
 		{
 			fprintf(stderr, "bad inode.\n");
 			return 1;
