5570189899deeef4f47df104f782d38727275b69
brgr-dev <berger1954@gmail.com>
2016-05-13 18:14:31 -0500

    update

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -277,7 +277,7 @@ int main(int argc, char* argv[]) {
 				}
 			}*/
 
-/*
+
 	for(i = mindatablock; i < maxblock; i++)
 	{
 		int addr = addrsinuse[i];
@@ -296,7 +296,7 @@ int main(int argc, char* argv[]) {
 			}
 		}
 	}
-		*/
+
 
 	for(i = 0; i < ninodes; i++)
 	{
