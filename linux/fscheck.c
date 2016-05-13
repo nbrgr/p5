b5726249ca6a1092653cf8f0003affdb7a3cf43f
brgr-dev <berger1954@gmail.com>
2016-05-13 14:51:26 -0500

    error codes in wrong order

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -200,12 +200,12 @@ int main(int argc, char* argv[]) {
 		{
 			if(addr == 0)
 			{
-				fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use.\n");
+				fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
 				return 1;
 			}
 			if(bitmap == 0)
 			{
-				fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+				fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use.\n");
 				return 1;
 			}
 		}
