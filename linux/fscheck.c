3a6bc878ce460fef34fe176f4676173454d5c085
kbstoner <kstoner246@gmail.com>
2016-05-13 13:41:56 -0500

    Update fscheck.c

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -126,7 +126,7 @@ int main(int argc, char* argv[]) {
 					for(j = 0; j < NINDIRECT; j++)
 					{
 						struct indirect* indiraddrs = (struct indirect*)&(blocks[inodes[i].addrs[NDIRECT]]);
-						fprintf(stderr, "indirect min: %i, max: %i, addr: %i\n", mindatablock, maxblock, indiraddrs->addrs[j]);
+						//fprintf(stderr, "indirect min: %i, max: %i, addr: %i\n", mindatablock, maxblock, indiraddrs->addrs[j]);
 						if((indiraddrs->addrs[j] < mindatablock || indiraddrs->addrs[j] >= maxblock) && indiraddrs->addrs[j] != 0) {
 							fprintf(stderr, "ERROR: bad address in inode.\n");
 							//printf("bad address\n");
