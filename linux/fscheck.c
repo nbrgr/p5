6e93638002d703b0abbdd8083fa5cc1588456755
kbstoner <kstoner246@gmail.com>
2016-05-13 15:24:12 -0500

    Update fscheck.c

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -172,7 +172,7 @@ int main(int argc, char* argv[]) {
 				        }
 					else if(strcmp(((struct dirent*)&(blocks[inodes[i].addrs[j]]))[k].name, "..") == 0) {
 						found++;
-						toparent = (struct dirent*)&(blocks[inodes[i].addrs[j]])[k];
+						toparent = ((struct dirent*)&(blocks[inodes[i].addrs[j]]))[k];
 					}
 				}
 			}
@@ -185,7 +185,7 @@ int main(int argc, char* argv[]) {
 				        	}
 						else if(strcmp(((struct dirent*)&(blocks[indiraddrs->addrs[j]]))[k].name, "..") == 0) {
 							found++;
-							toparent = (struct dirent*)&(blocks[inodes[i].addrs[j]])[k];
+							toparent = ((struct dirent*)&(blocks[inodes[i].addrs[j]]))[k];
 						}
 					}
 				}
