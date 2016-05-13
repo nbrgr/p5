8aa3cc715c3708e7efbe8456853a4c600a46c6e6
kbstoner <kstoner246@gmail.com>
2016-05-13 17:50:48 -0500

    Update fscheck.c

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -274,8 +274,8 @@ int main(int argc, char* argv[]) {
 		}
 		if(inodes[i].nlink != imrk[i]) {
 			printf("links: %i, counted: %i\n", inodes[i].nlink, imrk[i]);
-			fprintf(stderr, "ERROR: bad reference count for file.\n");
-			return 1;
+			//fprintf(stderr, "ERROR: bad reference count for file.\n");
+			//return 1;
 		}
 		if((inodes[i].type == T_DEV || inodes[i].type == T_DIR || inodes[i].type == T_FILE) && imrk[i] == 0)
 		{
