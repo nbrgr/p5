66b3a0677ccedef32f69fa59f2f36629684603a8
kbstoner <kstoner246@gmail.com>
2016-05-13 16:13:10 -0500

    Update fscheck.c

--- linux/fscheck.c
+++ linux/fscheck.c
@@ -216,7 +216,7 @@ int main(int argc, char* argv[]) {
 						}
 					}
 				}
-				printf("toparent->inum: i%\n", toparent->inum);
+				printf("toparent->inum: i%\n", (int)(toparent->inum));
 				printf("i: %i\n", i);
 				printf("index: %i\n", index);
 				if(&inodes[i] != &inodes[index]) {
